d3f913089a888861ae9b56fe65818f40aa1781b9
Xiiber <hihellomonster@gmail.com>
2022-03-07 19:49:44 -0600

    luisangelmaciel

---
+++ luisangelmaciel/icon-grind.js
@@ -0,0 +1,2 @@
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js " integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT " crossorigin="anonymous "></script>

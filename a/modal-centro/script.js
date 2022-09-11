3147c389e6a7d09b80c01fea351b182f14f7a601
luisangelmaciel <90217439+xbrpw@users.noreply.github.com>
2022-09-10 22:37:55 -0500

    Plus

--- a/modal-centro/script.js
+++
@@ -1,8 +0,0 @@
-var buttons = document.querySelectorAll(".toggle-button");
-var modal = document.querySelector("#modal");
-
-[].forEach.call(buttons, function(button) {
-  button.addEventListener("click", function() {
-    modal.classList.toggle("off");
-  })
-});

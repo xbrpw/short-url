782662ca80c5e686ded7d12e774b945368e9e13d
luisangelmaciel <90217439+xbrpw@users.noreply.github.com>
2022-11-07 04:28:27 -0600

    Zendimage

---
+++ Zendimage/src/main/java/com/example/Zendimage/ZendimageApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zendimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZendimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZendimageApplication.class, args);
+	}
+
+}
---
+++ Zendimage/src/test/java/com/example/Zendimage/ZendimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zendimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZendimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

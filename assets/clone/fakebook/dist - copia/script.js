9b6cd8d15ac1993f70e368800c635f7ae40e1076
luisangelmaciel <90217439+xbrpw@users.noreply.github.com>
2023-01-09 22:32:51 -0600

    +

---
+++ assets/clone/fakebook/dist - copia/script.js
@@ -0,0 +1,10 @@
+/*JS isn't my expertise 😉*/
+$(document).ready(function() {
+    $("#menuButton").on("click", function(){
+        $(".side-a").toggleClass("is-open");
+        $("html").toggleClass("is-nav-open");
+    });
+      $("#darkMode").on("click", function(){
+        $("html").toggleClass("is-dark");
+    });
+});

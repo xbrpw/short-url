fd60bf987f10870d44328cc59cf636268ef0f64f
luisangelmaciel <90217439+xbrpw@users.noreply.github.com>
2023-01-23 15:13:47 -0600

    +

---
+++ 2022/teclado-numerico/README.md
@@ -0,0 +1,5 @@
+# #JavaScript30 Day 1: JS Drum Kit
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/luisangelmaciel/pen/WNKMZPO](https://codepen.io/luisangelmaciel/pen/WNKMZPO).
+
+Taking the #JavaScript30 coding challenge! Here is day 1: JS Drum Kit!
---
+++ 2022/teclado-numerico/src/script.js
@@ -0,0 +1,34 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+window.addEventListener('mousedown', playSound);
+//tabs
+
+function openTab(evt, TabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("Tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += "";
+}
